7982f8caa3f953b9d457567d91c485b13318493f
jattajulia <jatta.kivihaka@helsinki.fi>
2022-09-18 20:25:52 +0300

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	password TEXT,
+	role INTEGER
+);
+
+CREATE TABLE material (
+	id SERIAL PRIMARY KEY,
+	title TEXT,
+	author TEXT,
+	year INTEGER,
+	type TEXT,
+	available BOOLEAN
+);
+
+CREATE TABLE reservations (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	material_id INTEGER REFERENCES material
+);
+
+CREATE TABLE borrowing_privileges,
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	suspended BOOLEAN
+);
+
+CREATE TABLE reviews (
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER REFERENCES users,
+	material_id INTEGER REFERENCES material,
+	rating INTEGER,
+	comment TEXT
+);
