7a9e9c2a4b2a9a274d95a3af3277f9b9dfb6a84b
jattajulia <jatta.kivihaka@helsinki.fi>
2022-09-21 20:54:59 +0300

    add more functions

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from app import app
 from flask import render_template, request, redirect
+import users
+import collection
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
@@ -14,3 +15,54 @@ def login():
 	if request.method == "POST":
 		username = request.form["username"]
 		password = request.form["password"]
+		if users.login(username, password):
+			return redirect("/")
+		else:
+			return render_template("error.html", message="Väärä käyttäjätunnus tai salasana")
+
+@app.route("/register", methods=["get", "post"])
+def register():
+	if request.method == "GET":
+		return render_template("register.html")
+
+	if request.method == "POST":
+		username = request.form["username"]
+		if len(username) < 1 or len(username) > 15:
+			return render_template("error.html", message="Käyttätunnuksen tulee olla 1-15 merkkiä pitkä")
+		password1 = request.form["password1"]
+		password2 = request.form["password2"]
+		if password1 != password2:
+			return render_template("error.html", message="Salasanat eroavat")
+		if password1 == "":
+			return render_template("error.html", message="Salasana ei voi olla tyhjä")
+		
+		role = request.form["role"]
+		if role not in ("1", "2"):
+			return render_template("error.html", message="Tuntematon käyttäjärooli")
+		if not users.register(username, password1, role):
+			return render_template("error.html", message="Rekisteröinti ei onnistunut")
+		return redirect("/") 
+
+@app.route("/logout")
+def logout():
+	users.logout()
+	return redirect("/")
+
+
+@app.route("/add", methods=["get", "post"])
+def add_material():
+	users.require_role(2)
+
+	if request.method == "GET":
+		return render_template("add.html")
+
+	if request.method == "POST":
+		title = request.form["title"]
+		author = request.form["author"]
+		year = int(request.form["year"])
+		language = request.form["language"]
+		
+		collection.add_material(title, author, year, language)
+		return redirect("/")
+
+
