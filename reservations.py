a313adee2b94e751ccc76a56eaaece3d15ad353c
jattajulia <jatta.kivihaka@helsinki.fi>
2022-09-24 18:13:37 +0300

    add function to get user's reservations

--- reservations.py
+++ reservations.py
@@ -1,10 +1,14 @@
 from db import db
 
 def make_reservation(material_id, user_id):
-	sql = """INSERT INTO reservations (user_id, material_id) VALUES (:material_id, :user_id)"""
-	db.session.execute(sql, {"material_id":material_id, "user_id":user_id})
+	sql = """INSERT INTO reservations (user_id, material_id) VALUES (:user_id, :material_id)"""
+	db.session.execute(sql, {"user_id":user_id, "material_id":material_id})
 	db.session.commit()
 
 def get_reservations(material_id):
-	sql = """SELECT COUNT(*) FROM reservations r WHERE r.id=:material_id"""
+	sql = """SELECT COUNT(*) FROM reservations r WHERE r.material_id=:material_id"""
 	return db.session.execute(sql, {"material_id": material_id}).fetchone()[0]
+
+def get_user_reservations(user_id):
+	sql = """SELECT m.id, m.title FROM material m, reservations r WHERE r.material_id=m.id AND r.user_id=:user_id"""
+	return db.session.execute(sql, {"user_id": user_id}).fetchall()
