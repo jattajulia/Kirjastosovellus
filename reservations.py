a7fc410db34815c64bc012f657a6d4ce599be4de
jattajulia <jatta.kivihaka@helsinki.fi>
2022-10-08 14:19:57 +0300

    add function to get most reserved titles

--- reservations.py
+++ reservations.py
@@ -12,3 +12,8 @@ def get_reservations(material_id):
 def get_user_reservations(user_id):
 	sql = """SELECT m.id, m.title FROM material m, reservations r WHERE r.material_id=m.id AND r.user_id=:user_id"""
 	return db.session.execute(sql, {"user_id": user_id}).fetchall()
+
+def get_most_reserved():
+	sql = """SELECT m.id, m.title, (SELECT COUNT(*) FROM reservations r WHERE r.material_id = m.id) count  FROM material m ORDER BY count DESC"""
+	return db.session.execute(sql).fetchmany(5)
+
