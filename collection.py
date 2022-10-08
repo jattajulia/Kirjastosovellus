e4faf4f99a09809700d303ed41d395603d4066ab
jattajulia <jatta.kivihaka@helsinki.fi>
2022-10-08 18:00:05 +0300

    move review functions to reviews.py

--- collection.py
+++ collection.py
@@ -38,12 +38,4 @@ def get_material(query):
 		return None
 	return db.session.execute(sql, {"query":"%"+query+"%"}).fetchall()
 
-def get_reviews(material_id):
-	sql = """SELECT u.name, r.rating, r.comment FROM reviews r, users u
-             WHERE r.user_id=u.id AND r.material_id=:material_id ORDER BY r.id"""
-	return db.session.execute(sql, {"material_id": material_id}).fetchall()
 
-def add_review(material_id, user_id, rating, comment):
-	sql = """INSERT INTO reviews (material_id, user_id, rating, comment) VALUES (:material_id, :user_id, :rating, :comment)"""
-	db.session.execute(sql, {"material_id":material_id, "user_id":user_id, "rating":rating, "comment":comment})
-	db.session.commit()
