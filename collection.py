a322f8302d95f68465fce0be4d0df2bfec2ef691
jattajulia <jatta.kivihaka@helsinki.fi>
2022-10-08 18:28:06 +0300

    add function to delete material

--- collection.py
+++ collection.py
@@ -5,6 +5,11 @@ def add_material(title, author, year, language):
 	db.session.execute(sql, {"title":title, "author":author, "year":year, "language":language})
 	db.session.commit()
 
+def delete_material(material_id):
+	sql = """DELETE FROM material WHERE id=:material_id"""
+	db.session.execute(sql, {"material_id": material_id})
+	db.session.commit()
+
 def get_material_info(material_id):
 	sql = """SELECT m.title, m.author, m.year, m.language FROM material m WHERE m.id=:material_id"""
 	return db.session.execute(sql, {"material_id": material_id}).fetchone()
@@ -14,7 +19,7 @@ def get_material_title(material_id):
 	return db.session.execute(sql, {"material_id": material_id}).fetchone()
 
 def get_all_material():
-	sql = "SELECT id, title FROM material ORDER BY title"
+	sql = """SELECT id, title FROM material ORDER BY title"""
 	return db.session.execute(sql).fetchall()
 
 def change_availability(material_id):
@@ -23,7 +28,7 @@ def change_availability(material_id):
 	db.session.commit()
 
 def get_new_material():
-	sql = "SELECT id, title FROM material ORDER BY id DESC"
+	sql = """SELECT id, title FROM material ORDER BY id DESC"""
 	return db.session.execute(sql).fetchmany(5)
 
 def get_material_availability(material_id):
@@ -31,7 +36,7 @@ def get_material_availability(material_id):
 	return db.session.execute(sql, {"material_id": material_id}).fetchone()[0]
 
 def get_material(query):
-	sql = "SELECT id, title FROM material WHERE title LIKE :query OR language LIKE :query OR author LIKE :query"
+	sql = """SELECT id, title FROM material WHERE title LIKE :query OR language LIKE :query OR author LIKE :query"""
 	result = db.session.execute(sql, {"query":"%"+query+"%"})
 	result_list = db.session.execute(sql, {"query":"%"+query+"%"}).fetchall()
 	if len(result_list) == 0:
