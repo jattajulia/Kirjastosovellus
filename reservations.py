9df7d42080bcf77b96769ecaff99f3b57e433180
jattajulia <jatta.kivihaka@helsinki.fi>
2022-10-08 14:36:59 +0300

    show author names on most reserved titles

--- reservations.py
+++ reservations.py
@@ -14,6 +14,6 @@ def get_user_reservations(user_id):
 	return db.session.execute(sql, {"user_id": user_id}).fetchall()
 
 def get_most_reserved():
-	sql = """SELECT m.id, m.title, (SELECT COUNT(*) FROM reservations r WHERE r.material_id = m.id) count  FROM material m ORDER BY count DESC"""
+	sql = """SELECT m.id, m.author, m.title, (SELECT COUNT(*) FROM reservations r WHERE r.material_id = m.id) count  FROM material m ORDER BY count DESC"""
 	return db.session.execute(sql).fetchmany(5)
 
